66e27680daf2ce44c4635622e8f0419b5e497ef0
unixguy <unixguy@fastmail.fm>
2016-05-31 14:10:56 -0700

    Added coverage for daaaangerous function

--- test_wordcount.py
+++ test_wordcount.py
@@ -54,3 +54,11 @@ def test_consume_4():
     assert chars == 12                     # includes whitespace in char count
     assert words == 5
     assert lines == 1
+
+
+def test_daaaangerous():
+    try:
+        wordcount_lib.daaaangerous()
+        assert False
+    except ZeroDivisionError as e:
+        assert True, 'Should thrown exception'
